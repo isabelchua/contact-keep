2f105fd2294be0e32b0f4d947e65dc90f2202046
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-08-13 21:42:31 -0700

    deploy edit

---
+++ client/build/precache-manifest.97f8ab5753a2ad37c7295b4b5fada795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e622bf4aa74ea17d99231a05f539e14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb80392f7e90ca13994",
+    "url": "/static/css/main.469bd671.chunk.css"
+  },
+  {
+    "revision": "13ae4be6ed4e4ad4041c",
+    "url": "/static/js/2.9c5e12dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9c5e12dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb80392f7e90ca13994",
+    "url": "/static/js/main.89e85c66.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
