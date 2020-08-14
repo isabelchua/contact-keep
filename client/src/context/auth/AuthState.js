5576bb2ed7f0f790fd6a4c938aa552ce5b5f30d4
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-08-13 20:17:09 -0700

    ready for deploy

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -98,7 +98,7 @@ const AuthState = props => {
 	const logout = () => dispatch({ type: LOGOUT });
 
 	//clear errors
-	const clearErrors = () => dispatch({ type: clearErrors });
+	const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
 	return (
 		<AuthContext.Provider
